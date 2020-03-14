dd6a33bec211ba857fe3f5cd030d16e4e74a4b84
viviansunmola1 <viviansunmola@gmail.com>
2020-03-14 17:58:52 +0000

    Comparing algorithms

--- machinelearning.py
+++ machinelearning.py
@@ -60,6 +60,45 @@ print(dataset.groupby('class').size())
 dataset.plot(kind='box', subplots=True, layout=(2,2), sharex=False, sharey=False)
 pyplot.show()
 
+# histograms
+dataset.hist()
+pyplot.show()
+
+# scatter plot matrix
+scatter_matrix(dataset)
+pyplot.show()
+
+array = dataset.values
+X = array[:,0:4]
+y = array[:,4]
+X_train, X_validation, Y_train, Y_validation = train_test_split(X, y, test_size=0.20, random_state=1)
+
+# Spot Check Algorithms
+models = []
+models.append(('LR', LogisticRegression(solver='liblinear', multi_class='ovr')))
+models.append(('LDA', LinearDiscriminantAnalysis()))
+models.append(('KNN', KNeighborsClassifier()))
+models.append(('CART', DecisionTreeClassifier()))
+models.append(('NB', GaussianNB()))
+models.append(('SVM', SVC(gamma='auto')))
+# evaluate each model in turn
+results = []
+names = []
+for name, model in models:
+	kfold = StratifiedKFold(n_splits=10, random_state=1, shuffle=True)
+	cv_results = cross_val_score(model, X_train, Y_train, cv=kfold, scoring='accuracy')
+	results.append(cv_results)
+	names.append(name)
+	print('%s: %f (%f)' % (name, cv_results.mean(), cv_results.std()))
+
+
+
+pyplot.boxplot(results, labels = names)
+pyplot.title('Algorithm Comparison')
+pyplot.show()
+
+
+
 
 
 
