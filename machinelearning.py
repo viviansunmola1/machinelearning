d844a88890afee2a766309e3d9c944527ed0ff65
viviansunmola1 <viviansunmola@gmail.com>
2020-03-14 17:05:18 +0000

    creating univariate plots

--- machinelearning.py
+++ machinelearning.py
@@ -37,18 +37,28 @@ from sklearn.svm import SVC
 
 
 
+
+# summarize the data
+from pandas import read_csv
 # Load dataset
 url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv"
 names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
 dataset = read_csv(url, names=names)
-
-
-
+# shape
 print(dataset.shape)
+# head
 print(dataset.head(20))
+# descriptions
+print(dataset.describe())
+# class distribution
+print(dataset.groupby('class').size())
 
 
 
+
+# box and whisker plots
+dataset.plot(kind='box', subplots=True, layout=(2,2), sharex=False, sharey=False)
+pyplot.show()
 
 
 
